658df84b0f909f53c8ab02e77fbffd55e0f9ff67
Jae Seok Lee <jslee062749@gmail.com>
2019-04-27 00:04:39 +0200

    refactoring functions... still workin fine

--- src2/command_buffers.c
+++ src2/command_buffers.c
@@ -6,7 +6,7 @@
 /*   By: jaelee <jaelee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/22 11:21:15 by jaelee            #+#    #+#             */
-/*   Updated: 2019/04/26 23:39:53 by jaelee           ###   ########.fr       */
+/*   Updated: 2019/04/27 00:03:42 by jaelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	create_command_buffers(t_vulkan *vulkan)
 {
+	uint32_t	i;
+
 	vulkan->command_buffers = (VkCommandBuffer*)malloc(vulkan->swapchain_image_count * sizeof(VkCommandBuffer));
 	VkCommandBufferAllocateInfo	allocate_info = {};
 	allocate_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
@@ -22,31 +24,35 @@ void	create_command_buffers(t_vulkan *vulkan)
 	allocate_info.commandBufferCount = vulkan->swapchain_image_count;
 	vkAllocateCommandBuffers(vulkan->logical_device, &allocate_info, vulkan->command_buffers);
 
-	for (int i=0; i < vulkan->swapchain_image_count; i++) {
-	VkCommandBufferBeginInfo	begin_info = {};
-	begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-// 	begin_info.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
-	begin_info.flags = VK_COMMAND_BUFFER_USAGE_SIMULTANEOUS_USE_BIT;
-	vkBeginCommandBuffer(vulkan->command_buffers[i], &begin_info);
-	VkClearColorValue		color = {48.f / 255.f, 10.f / 255.f,  36.f / 255.f, 1};
+	i = 0;
+	while (i < vulkan->swapchain_image_count)
+	{
+		VkCommandBufferBeginInfo	begin_info = {};
+		begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+	// 	begin_info.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+		begin_info.flags = VK_COMMAND_BUFFER_USAGE_SIMULTANEOUS_USE_BIT;
+		vkBeginCommandBuffer(vulkan->command_buffers[i], &begin_info);
+		VkClearColorValue		color = {48.f / 255.f, 10.f / 255.f,  36.f / 255.f, 1};
 
-	VkClearValue			clear_color = {color};
-	VkRenderPassBeginInfo	begin_pass = {};
-	begin_pass.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-	begin_pass.renderPass = vulkan->renderpass;
-	begin_pass.framebuffer = vulkan->frame_buffers[i];
-	begin_pass.renderArea.extent.width = vulkan->swapchain_extent.width;
-	begin_pass.renderArea.extent.height = vulkan->swapchain_extent.height;
-	begin_pass.clearValueCount = 1;
-	begin_pass.pClearValues = &clear_color;
-		vkCmdBeginRenderPass(vulkan->command_buffers[i], &begin_pass, VK_SUBPASS_CONTENTS_INLINE);
+		VkClearValue			clear_color = {color};
+		VkRenderPassBeginInfo	begin_pass = {};
+		begin_pass.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+		begin_pass.renderPass = vulkan->renderpass;
+		begin_pass.framebuffer = vulkan->frame_buffers[i];
+		begin_pass.renderArea.extent.width = vulkan->swapchain_extent.width;
+		begin_pass.renderArea.extent.height = vulkan->swapchain_extent.height;
+		begin_pass.clearValueCount = 1;
+		begin_pass.pClearValues = &clear_color;
+			vkCmdBeginRenderPass(vulkan->command_buffers[i], &begin_pass, VK_SUBPASS_CONTENTS_INLINE);
 
-			vkCmdBindPipeline(vulkan->command_buffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, vulkan->graphics_pipeline);
-			vkCmdDraw(vulkan->command_buffers[i], 3, 1, 0, 0);
+				vkCmdBindPipeline(vulkan->command_buffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, vulkan->graphics_pipeline);
+				vkCmdDraw(vulkan->command_buffers[i], 3, 1, 0, 0);
 
-		vkCmdEndRenderPass(vulkan->command_buffers[i]);
+			vkCmdEndRenderPass(vulkan->command_buffers[i]);
 
-	vkEndCommandBuffer(vulkan->command_buffers[i]); }
+		vkEndCommandBuffer(vulkan->command_buffers[i]);
+		i++;
+	}
 }
 
 void	create_command_pools(t_vulkan *vulkan)
